8e73361e13d6cd7842e03b3eea2d0454693b281f
srigiri02 <102649604+srigiri02@users.noreply.github.com>
2022-03-30 13:17:34 +0530

    Update sri.php

--- web/sri.php
+++ web/sri.php
@@ -2,7 +2,12 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', 'root', '','db_connect');
+$host        = "host=ec2-54-173-77-184.compute-1.amazonaws.com";
+$port        = "port=5432";
+$dbname      = "dbname = d2idq0j9577bq7";
+$credentials = "user = uhstuthvacrjem password=cff386185e984a6151825844d5f8051e460bceb92558ed7e9758c5fdea05092d";
+
+$con = pg_connect( "$host $port $dbname $credentials"  );
 
 // get the post records
 $txtName = $_POST['txtName'];
@@ -11,14 +16,17 @@ $txtPhone = $_POST['txtPhone'];
 $txtMessage = $_POST['txtMessage'];
 
 // database insert SQL code
-$sql = "INSERT INTO `tbl_contact2` (`fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+$sql = "INSERT INTO tbl_contact(fldName, fldEmail, fldPhone, fldMessage) VALUES ('$txtName', '$txtEmail', '$txtPhone', '$txtMessage');";
+//echo $sql;
 
 // insert in database 
-$rs = mysqli_query($con, $sql);
+$rs = pg_query($con, $sql);
 
 if($rs)
 {
-	echo "Contact Records Inserted";
+  echo "Contact Records Inserted";
+}else{
+  echo "Contact Not Inserted";
 }
 
 ?>
